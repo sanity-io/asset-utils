fbe79e110adf23b9deac8f01aa9c52d55864f97f
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-10-03 12:04:13 -0700

    docs: remove unnecessary html modification steps

--- scripts/generateReadme.ts
+++ scripts/generateReadme.ts
@@ -359,71 +359,6 @@ function modifyHtmlDocs() {
   const htmlPath = path.join(basePath, 'docs', 'index.html')
   const $ = cheerio.load(fs.readFileSync(htmlPath))
 
-  // Get constants that start with `try` and treat them as functions
-  $('.tsd-kind-variable > a[href*="#try"]')
-    .parent()
-    .removeClass('tsd-kind-variable')
-    .addClass('tsd-kind-function')
-
-  // Move constants that are functions from the constants blocks to the functions block
-  const fnsInConstIndex = $('.tsd-index-section h3:contains("Variables")')
-    .closest('section')
-    .find('.tsd-kind-function')
-
-  // Move the functions to the top of the index
-  const fnsIndexSection = $('.tsd-index-section h3:contains("Functions")').closest('section')
-  fnsIndexSection.parent().prepend(fnsIndexSection)
-
-  const fnsListSection = $('.tsd-member-group h2:contains("Functions")').closest(
-    '.tsd-member-group',
-  )
-  fnsListSection.parent().find('.tsd-index-group').after(fnsListSection)
-
-  // Move constants that are functions from the variables list to the functions list
-  const fnsInList = $('.tsd-member-group h2:contains("Variables")')
-    .closest('section')
-    .find('a[name^="try"]')
-    .closest('.tsd-member')
-    .removeClass('tsd-kind-variable')
-    .addClass('tsd-kind-function')
-    .appendTo(fnsListSection)
-
-  // Find all "const" flags in the functions list, and remove them
-  fnsListSection.find('.ts-flagConst').remove()
-  fnsInList.find('a[name^="try"]').each(function () {
-    const a = $(this)
-    const target = a.attr('name')?.replace(/^try/, '')
-    const section = a.closest('section')
-    const parent = a.closest('.tsd-member-group').find(`a[name="${target}"]`).closest('section')
-    const parentSig = parent.find('.tsd-signatures')
-    const signature = parentSig.clone()
-    signature
-      .find('.tsd-signature')
-      .append(
-        '<span class="tsd-signature-symbol"> | </span><span class="tsd-signature-type">undefined</span>',
-      )
-
-    section.find('.tsd-signature').replaceWith(signature)
-  })
-
-  // Move classes to the top of the index
-  const classesIndexSection = $('.tsd-index-section h3:contains("Classes")').closest('section')
-  classesIndexSection.parent().append(classesIndexSection)
-
-  // Append the "try" functions (previously constants) to the functions list
-  fnsIndexSection.find('ul').append(fnsInConstIndex)
-
-  // Move the functions to the top of the navigation
-  const nav = $('.tsd-navigation ul')
-  const fns = orderNodesByName(nav.find('.tsd-kind-function'), $)
-  nav.prepend(fns)
-
-  // Don't reference "globals.html", use index!
-  $('a[href^="globals.html"]').each(function () {
-    const el = $(this)
-    el.attr('href', el.attr('href')?.replace('globals.html', 'index.html'))
-  })
-
   // If encountering (broken) @link tags - throw
   $(':contains("@link")').each(function () {
     const html = $(this).html() || ''
@@ -432,24 +367,5 @@ function modifyHtmlDocs() {
     })
   })
 
-  // Remove filters, as they are noops in our case
-  $('#tsd-filter').html(outdent`
-    <a href="https://github.com/sanity-io/asset-utils" style="display: flex;">
-      <svg style="width: 24px; margin-right: 5px;" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub icon</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
-      Fork me on GitHub
-    </a>
-  `)
-
   fs.writeFileSync(htmlPath, $.html())
-}
-
-function orderNodesByName(nodes: ReturnType<cheerio.CheerioAPI>, $: cheerio.CheerioAPI) {
-  return nodes
-    .toArray()
-    .slice()
-    .sort((a, b) => {
-      const aName = $(a).text().trim()
-      const bName = $(b).text().trim()
-      return aName.localeCompare(bName)
-    })
 }
