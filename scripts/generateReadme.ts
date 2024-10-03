49c6feb8fe8481236f2562038f5e9aee83df2990
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-10-03 11:55:04 -0700

    fix: prevent readme template from being published

--- scripts/generateReadme.ts
+++ scripts/generateReadme.ts
@@ -33,8 +33,8 @@ let allNodes: Array<JSONOutput.DeclarationReflection>
 let functions: Array<JSONOutput.DeclarationReflection>
 
 const mdFilename = 'README.md'
-const mdTemplate = fs.readFileSync(path.join(basePath, 'README.template.md'), 'utf8')
-const mdBase = `<!-- This file is AUTO-GENERATED, edit README.template.md or tweak scripts/generateReadme.js -->\n${mdTemplate}`
+const mdTemplate = fs.readFileSync(path.join(basePath, '_README.template.md'), 'utf8')
+const mdBase = `<!-- This file is AUTO-GENERATED, edit _README.template.md or tweak scripts/generateReadme.ts -->\n${mdTemplate}`
 
 generateHtmlDocs()
 modifyHtmlDocs()
